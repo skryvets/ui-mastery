9171401734879f69d211646194d6a0f1c3b6af9d
sergeome <sergeome@gmail.com>
2017-03-09 22:06:34 -0600

    Code spacing adjustment

--- gulpfile.js
+++ gulpfile.js
@@ -13,60 +13,60 @@ gulp.task('default', function () {
 
 //Init server and connect
 gulp.task('connect', function () {
-	connect.server({
-		root: './',
-		livereload: true
-	});
+    connect.server({
+        root: './',
+        livereload: true
+    });
 });
 
 //Reload on any HTML changes
 gulp.task('reload', function () {
-	gulp.src('./*.html')
-	.pipe(connect.reload());
-	
+    gulp.src('./*.html')
+    .pipe(connect.reload());
+    
 });
 
 //Compile SCSS and reload
 gulp.task('sass', function () {
-	return gulp.src('./src/scss/style.scss')
-	.pipe(sourcemaps.init())
-	.pipe(sass().on('error', sass.logError))
-	.pipe(sourcemaps.write())
-	.pipe(gulp.dest('./dist'))
-	.pipe(connect.reload());
+    return gulp.src('./src/scss/style.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist'))
+    .pipe(connect.reload());
 });
 
 //Concat, Minify JS and reload
 gulp.task('js', function () {
-	return gulp.src('./src/js/main.js')
-	.pipe(gulp.dest('./dist'))
-	.pipe(connect.reload());
+    return gulp.src('./src/js/main.js')
+    .pipe(gulp.dest('./dist'))
+    .pipe(connect.reload());
 });
 
 //Gulp Watcher
 gulp.task('watch', function () {
-	gulp.watch(['./**/*.html'], ['reload']);
-	gulp.watch(['./src/scss/**/*.scss'], ['sass']);
-	gulp.watch(['./src/js/main.js'], ['js']);
+    gulp.watch(['./**/*.html'], ['reload']);
+    gulp.watch(['./src/scss/**/*.scss'], ['sass']);
+    gulp.watch(['./src/js/main.js'], ['js']);
 });
 
 
 //Production Compilation: minify and remove source maps
 gulp.task('prod-sass', function () {
-	return gulp.src('./src/scss/style.scss')
-	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-	.pipe(gulp.dest('./dist'))
+    return gulp.src('./src/scss/style.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('./dist'))
 });
 
 gulp.task('prod-js', function () {
-	return gulp.src('./src/js/main.js')
-	.pipe(uglify())
-	.pipe(gulp.dest('./dist'))
+    return gulp.src('./src/js/main.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist'))
 });
 
 gulp.task('prod', ['prod-sass', 'prod-js'], function () {
-	return gulp.src(['./css/*', '!./css/style.css'], {read: false})
-	.pipe(clean(({force: true})));
+    return gulp.src(['./css/*', '!./css/style.css'], {read: false})
+    .pipe(clean(({force: true})));
 });
 
 //Default Task
