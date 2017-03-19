5a8c68471514596674a5bb4f4c6edc27dace1036
sergeome <sergeome@gmail.com>
2017-03-18 23:44:27 -0500

    Update (main.js): Update var to es6 declaration

--- src/js/main.js
+++ src/js/main.js
@@ -5,12 +5,12 @@ import jQuery from './vendor/jquery-3.2.0.min.js';
     
     "use strict";
     
-    var JS = function () {
+    let JS = function () {
         this.initExample();
     };
     
     JS.prototype.initExample = function () {
-        console.log("Hello world from es6");
+        console.log("Hello world from es6!");
     };
     
     new JS();
