04f2d8df0b683fd3e422eb70110af446d6ff802d
sergeome <sergeome@gmail.com>
2017-03-18 23:42:04 -0500

    Fix (main.js): Add Jquery import

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
-/*
-Main JavaScript File
-*/
+import $ from './vendor/jquery-3.2.0.min.js';
+import jQuery from './vendor/jquery-3.2.0.min.js';
 
 (function ($) {
     
@@ -11,10 +10,9 @@ Main JavaScript File
     };
     
     JS.prototype.initExample = function () {
-        console.log("Hello world");
+        console.log("Hello world from es6");
     };
     
     new JS();
     
 }(jQuery));
-
